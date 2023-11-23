092ea6b0c48dd00e1a0f17ce8b6a78defaaa057f
Aleksei <77012830+al-kos@users.noreply.github.com>
2023-11-23 11:09:30 +0300

    version_2

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-// Доступ к элементам
 let imgBox = document.getElementById('imgBox')
 let qrImage = document.getElementById('qrImage')
 let qrText = document.getElementById('qrText')
 
-// Функция генерации QR-кода
-function generateQR() {
-  // Проверка вводимого значения на пустоту
+async function generateQR() {
   if (qrText.value.length > 0) {
-    // Добавление введенного значения в API для формирования картинки кода
-    qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`
-    imgBox.classList.add('show-img')
+    let response = await fetch(
+      `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`,
+    )
+    if (response.ok) {
+      qrImage.src = response.url
+      imgBox.classList.add('show-img')
+    } else {
+      alert('Ошибка HTTP: ' + response.status)
+    }
   } else {
     qrText.classList.add('error')
     setTimeout(() => {
