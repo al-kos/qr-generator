d753b6d6a3556dd42f18cadda7475d46af51e446
Alex Kos <kosenkoaleksey34@gmail.com>
2023-11-23 19:14:04 +0300

    версия с отображением текста при ошибке загрузки, логирование в консоль оставил для удобства проверки и демонстрации работоспособности

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ function generateQR() {
   if (qrText.value.length > 0) {
     qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`
     qrImage.onload = () => console.log(`Изображение загружено`)
-    qrImage.onerror = () => console.log('Ошибка во время загрузки изображения')
+    qrImage.onerror = () => {
+      const errorMessage = 'Ошибка во время загрузки изображения'
+      const errorDiv = document.createElement('div')
+      errorDiv.innerText = errorMessage
+      imgBox.appendChild(errorDiv)
+      console.log('Ошибка во время загрузки изображения')
+    }
     imgBox.classList.add('show-img')
   } else {
     qrText.classList.add('error')
